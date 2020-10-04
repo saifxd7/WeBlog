bc02c321a5d3383044ff1ed10efcd0da9d9551ab
kingsw0rd <you@example.com>
2020-10-04 15:56:45 +0530

    initial commit

---
+++ backend/build/precache-manifest.558113e59a826544303e8c2e2533fe15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed944100168494463e71b54fb14299d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b429f61d743eec1615a",
+    "url": "/static/js/2.ed258354.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed258354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c548cbb2ff5609d9a3",
+    "url": "/static/js/main.177ae9c1.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
